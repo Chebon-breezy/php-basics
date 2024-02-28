a6bad149564f0616d3c565deb485227b34264b1a
Brian Chebon <brianlchebon@gmail.com>
2024-02-28 12:20:35 +0300

    added php object

---
+++ includes/newclass.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+class Newclass {
+    //properties and methods goes here
+    public $info = "This is info";
+} 
+
+$object = new Newclass;
+var_dump($object);
---
+++ procedural/php forms/switch.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $bool = true;
+    $a = 1;
+    $b = 4;
+
+    switch($a){
+        case 1:
+            echo "first case is correct";
+            break;
+        case 2:
+            echo "first case is correct";
+            break;
+        default:
+            echo "None are true";
+                
+    }
+
+    ?>
+</body>
+</html>
