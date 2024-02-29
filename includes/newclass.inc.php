91565438104f1f2a459a0e1600eac3c258dfe599
Brian Chebon <brianlchebon@gmail.com>
2024-02-29 15:09:15 +0300

    added person class

--- includes/newclass.inc.php
+++ includes/newclass.inc.php
@@ -3,7 +3,20 @@
 class Newclass {
     //properties and methods goes here
     public $info = "This is info";
-} 
+}
+
+class person {
+    protected $first = "Brian";
+    private $last = "Chebon";
+    private $age = "25";
+}
+
+class pet extends person {
+    public function owner(){
+        $a = $this->first;
+        return $a;
+    }
+}
 
 $object = new Newclass;
-var_dump($object);
+var_dump($object); 
